3e920636fd416a32fa8e318e99c04d76046558cd
Jean Valverde <jeanv@catalyst.net.nz>
2017-09-25 00:50:10 +0000

    Add other env possible settings overrides

--- web/sites/default/settings.dev.php
+++ web/sites/default/settings.dev.php
@@ -12,6 +12,21 @@ $config['config_split.config_split.dev_config']['status'] = TRUE;
 $settings['file_private_path'] = 'sites/default/files/private';
 $settings['simple_environment_indicator'] = 'DarkGreen Dev';
 
+
+// Proxy settings.
+// $settings['http_client_config']['proxy']['http'] = 'http://My_WEB_PROXY:8080';
+// $settings['http_client_config']['proxy']['https'] = 'http://My_WEB_PROXY:8080';
+// $settings['http_client_config']['proxy']['no'] = [
+//   '127.0.0.1',
+//   'localhost',
+//   'SPECIFIC_IP',
+// ];
+
+// Add specific trusted host
+// $settings['trusted_host_patterns'][] = '^MY_OTHER_NAME_OR_IP$';
+
+// Drupal default dev settings from example.settings.local.php.
+
 // Assertions
 assert_options(ASSERT_ACTIVE, TRUE);
 \Drupal\Component\Assertion\Handle::register();
--- web/sites/default/settings.prod.php
+++ web/sites/default/settings.prod.php
@@ -11,3 +11,15 @@ $config['config_split.config_split.dev_config']['status'] = FALSE;
 // Local settings.
 $settings['file_private_path'] = 'sites/default/files/private';
 $settings['simple_environment_indicator'] = 'DarkRed PROD';
+
+// Proxy settings.
+// $settings['http_client_config']['proxy']['http'] = 'http://My_WEB_PROXY:8080';
+// $settings['http_client_config']['proxy']['https'] = 'http://My_WEB_PROXY:8080';
+// $settings['http_client_config']['proxy']['no'] = [
+//   '127.0.0.1',
+//   'localhost',
+//   'SPECIFIC_IP',
+// ];
+
+// Add specific trusted host
+// $settings['trusted_host_patterns'][] = '^MY_OTHER_NAME_OR_IP$';
