2f9f45149af293c2e0c7bd5950f6672a31e3b31f
Jean Valverde <jeanvalverde@catalyst.net.nz>
2018-09-05 10:44:26 +1200

    Update Drupal project template, add Bootstrat starterkit script.

--- README.md
+++ README.md
@@ -2,39 +2,35 @@
 
 Based on [Composer template for Drupal projects](https://github.com/drupal-composer/drupal-project).
 
-## What's added ?
+## What's added?
 
-* Drupal basic config with Dev / Prod environment, see [Workflow readme](config/README.md).
-* Base contrib modules, Core patches.
-* Third party libraries download with https://asset-packagist.org.
+* Drupal basic config with Dev / Prod environment, see [Workflow readme](config/README.md)
+* Base contrib modules, Core patches
+* Third party libraries download with https://asset-packagist.org
+* Bootstrap Sass sub theme
 
 ## Install
 
-#### Requirements
+### Requirements
 
 Package creation require [Composer 1.6+](https://getcomposer.org) with [Php 7+](http://php.net/) and Php modules needed for composer. To create the styles files you need if using Bootsrap Sass  [Compass 1+](http://compass-style.org/install)
 
 Recommended:
+
 * [Composer prestissimo](https://github.com/hirak/prestissimo)
 
-```
-composer global require "hirak/prestissimo:^0.3"
-```
+    composer global require "hirak/prestissimo:^0.3"
 
-#### Grab code and libraries
+### Grab code and libraries
 
 Clone this project locally in your web root folder.
-```
-git clone git@github.com:Mogtofu33/drupal-composer-advanced-template.git drupal
-```
+
+    git clone git@github.com:Mogtofu33/drupal-composer-advanced-template.git drupal
 
 Download project code, from this folder run
-```
-composer install --no-dev
-```
 
-Optional: Install any bootstrap starter kit
-* https://drupal-bootstrap.org/api/bootstrap/docs%21Sub-Theming.md/group/sub_theming/8
+    cd drupal
+    composer install --no-dev
 
 Set **/web** as root of your host (Apache).
 
@@ -48,28 +44,23 @@ Other folders (eg: vendor) should be accessible by Webserver user and not from H
 
 * Drush command installation to run from **web** folder, change uppercase variables to match your environment:
 
-```
-../vendor/bin/drush si config_installer \
-config_installer_sync_configure_form.sync_directory="../config/sync" \
---db-url=mysql://DB_USER:DB_PASS@DB_HOST/DB_NAME \
---account-name=admin \
---account-pass=ADMIN_PASSWORD \
---account-mail=ADMIN_MAIL@MAIL.COM
-```
+    ../vendor/bin/drush si config_installer \
+        config_installer_sync_configure_form.sync_directory="../config/sync" \
+        --db-url=mysql://DB_USER:DB_PASS@DB_HOST/DB_NAME \
+        --account-name=admin \
+        --account-pass=ADMIN_PASSWORD \
+        --account-mail=ADMIN_MAIL@MAIL.COM
+
 
 * Edit _web/sites/default/settings.php_, at the end of the file uncomment
   settings.local.php inclusion.
 
-```
-  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-    include $app_root . '/' . $site_path . '/settings.local.php';
-  }
-```
+    if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+      include $app_root . '/' . $site_path . '/settings.local.php';
+    }
 
 * Copy and rename _web/sites/default/example.settings.local.php_ to
   _web/sites/default/example.settings.local.php_ and edit to adapt environment
   switch.
 
-```
-sudo cp web/sites/default/example.settings.local.php web/sites/default/settings.local.php
-```
+    sudo cp web/sites/default/example.settings.local.php web/sites/default/settings.local.php
---
+++ scripts/composer/BootstrapSass.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalProject\composer\ScriptHandler.
+ */
+
+namespace DrupalProject\composer;
+
+use Composer\Script\Event;
+use DrupalFinder\DrupalFinder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class BootstrapSass {
+
+  /**
+   * Create Bootstrap Sass subtheme.
+   *
+   * see https://drupal-bootstrap.org/api/bootstrap/starterkits%21sass%21README.md/group/sub_theming_sass/8
+   * Assume we already have bootstrap sources from composer.
+   */
+  public static function create(Event $event) {
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+    $finder = new Finder();
+
+    $themeName = "bootstrap_sass";
+    $themeTitle = "Bootstrap Sass";
+    $bootstrapVersion = "3.3.7";
+    $configRb = "https://gitlab.com/mog33/gitlab-ci-drupal/snippets/1751092/raw";
+    $customFolder = $drupalRoot . '/themes/custom/' . $themeName;
+    $contribFolder = $drupalRoot . '/themes/contrib/bootstrap';
+
+    // If config.rb exist mean we can stop here.
+    if ($fs->exists($customFolder . '/config.rb')) {
+      echo "[info] Bootstrap Sass theme exist, skip.\n";
+      return;
+    }
+
+    // Create Bootstrap subtheme from starterkit.
+    $fs->mkdir($customFolder);
+    $fs->mirror($contribFolder . '/starterkits/sass', $customFolder);
+
+    // Copy and adapt config to get a default block position.
+    $fs->mkdir($customFolder . '/config/optional');
+    $fs->mirror($contribFolder . '/config/optional', $customFolder . '/config/optional');
+    $finder->files()->in($customFolder . '/config/optional/');
+    foreach ($finder as $file) {
+      $content = $file->getContents();
+      $content = str_replace('id: bootstrap', 'id: ' . $themeName, $content);
+      $content = str_replace('theme: bootstrap', 'theme: ' . $themeName, $content);
+      $content = str_replace('- bootstrap', '- ' . $themeName, $content);
+      $fs->dumpFile($file->getPathname(), $content);
+      $new_filename = str_replace('block.bootstrap', 'block.' . $themeName, $file->getFilename());
+      $fs->rename($file->getPathname(), $file->getPath() . '/' . $new_filename);
+    }
+
+    // Adapt files names and content for the starterkit.
+    $files = [
+      '/THEMENAME.starterkit.yml' => '/' . $themeName . '.info.yml',
+      '/THEMENAME.libraries.yml' => '/' . $themeName . '.libraries.yml',
+      '/THEMENAME.theme' => '/' . $themeName . '.theme',
+      '/config/install/THEMENAME.settings.yml' => '/config/install/' . $themeName . '.settings.yml',
+      '/config/schema/THEMENAME.schema.yml' => '/config/schema/' . $themeName . '.schema.yml',
+    ];
+    foreach ($files as $source => $target) {
+      $content = file_get_contents($customFolder . $source);
+      $content = str_replace('THEMETITLE', 'id: ' . $themeName, $content);
+      $content = str_replace('THEMENAME', 'theme: ' . $themeTitle, $content);
+      $fs->dumpFile($customFolder . $target, $content);
+      $fs->remove($customFolder . $source);
+    }
+
+    // We need a config file for compiling.
+    $config = file_get_contents($configRb);
+    $fs->dumpFile($customFolder . '/config.rb', $config);
+
+    // Inform about next steps.
+    echo "[info] You need to run compass compile, ie:\n  compass compile " . $customFolder . "\n\n";
+    echo "And enable the new theme Boostrap Sass after Drupal installtion.\n";
+
+  }
+
+}
