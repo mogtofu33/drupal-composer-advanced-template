84b85e60f8706ad740d71770f727b4e5a0bef410
Jean Valverde <moimog33@gmail.com>
2018-12-11 06:04:40 +0000

    Update .gitlab-ci.yml, .gitlab-ci/settings-ci.php, .gitlab-ci/.env files

--- .gitlab-ci/settings-ci.php
+++ .gitlab-ci/settings-ci.php
@@ -1,6 +1,6 @@
 <?php
 
-$settings['hash_salt'] = '3U5rP_ZgCTzvGt4sfWvVTL6H2kvtBkYaU8ExLxrobmvV-hBh-ZaQ_PgEAANkZq--LiBsQ60M_6w';
+$settings['hash_salt'] = '3U5rP_Z6H2kvtBkYaU8ExLxrobmvV-hBh-ZaQ_gCTzvGt4sfWvVTLPgEAANkZq--LiBsQ60M_6w';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 $settings['file_scan_ignore_directories'] = [
@@ -11,16 +11,16 @@ $settings['entity_update_batch_size'] = 50;
 
 $config_directories['sync'] = '../config/sync';
 
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'root',
-  'password' => '',
-  'prefix' => '',
-  'host' => 'mariadb',
-  'port' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+$databases['default']['default'] = [
+  'database' => getenv('MYSQL_DATABASE'),
   'driver' => 'mysql',
-);
+  'host' => getenv('MYSQL_HOSTNAME'),
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'password' => getenv('MYSQL_PASSWORD'),
+  'port' => getenv('MYSQL_PORT'),
+  'prefix' => '',
+  'username' => getenv('MYSQL_USER'),
+];
 
 # https://www.drupal.org/project/drupal/issues/2867042
 $settings['file_chmod_directory'] = 02775;
