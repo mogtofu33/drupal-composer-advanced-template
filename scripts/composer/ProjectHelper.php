8d3f32bfda36c9456fc87b1a4296b86fc7083780
Jean Valverde <moimog33@gmail.com>
2020-12-06 19:08:57 +0100

    fix(): support local settings

--- scripts/composer/ProjectHelper.php
+++ scripts/composer/ProjectHelper.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DrupalProject\composer;
 
 use Composer\Script\Event;
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 use DrupalFinder\DrupalFinder;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -20,44 +24,52 @@ final class ProjectHelper {
    * @param \Composer\Script\Event $event
    *   The composer scripts Command Events.
    */
-  public static function createRequiredFiles(Event $event) {
+  public static function createRequiredFiles(Event $event): void {
     $fs = new Filesystem();
     $drupalFinder = new DrupalFinder();
     $io = $event->getIO();
 
-    $drupalDocRoot = getcwd();
-    $drupalFinder->locateRoot(getcwd());
+    $drupalDocRoot = \getcwd();
+    $drupalFinder->locateRoot($drupalDocRoot);
     $drupalRoot = $drupalFinder->getDrupalRoot();
-    $drupalSettings = $drupalRoot . '/sites/default';
+    $drupalSettings = $drupalRoot . '/sites';
+
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->safeLoad();
 
     $files = [
-      $drupalSettings . '/default.settings.php' => $drupalSettings . '/settings.php',
-      $drupalDocRoot . '/.env.example' => $drupalDocRoot . '/.env',
+      $drupalRoot . '/sites/default/default.settings.php' => $drupalRoot . '/sites/default/settings.php',
+      $drupalRoot . '/.env.example' => $drupalRoot . '/.env',
     ];
+
+    if ('dev' === $_ENV['SETTINGS_ENVIRONMENT']) {
+      $io->write('<info>Dev environment detected.</info>');
+      $files[$drupalRoot . '/sites/example.settings.local.php'] = $drupalRoot . '/sites/default/settings.local.php';
+    }
 
     foreach ($files as $src => $dest) {
       if ($fs->exists($src)) {
         if ($fs->exists($dest)) {
-          $io->write(sprintf('<info>[SKIP] Existing file %s</info>', $dest));
+          $io->write(\sprintf('<info>[SKIP] Existing file %s</info>', $dest));
         }
         else {
-          $io->write(sprintf('<info>Create %s from %s</info>', $dest, $src));
+          $io->write(\sprintf('<info>Create %s from %s</info>', $dest, $src));
           $fs->copy($src, $dest);
         }
       }
       else {
-        $io->warning(sprintf('Missing file %s', $src));
+        $io->warning(\sprintf('Missing file %s', $src));
       }
     }
 
     if ($fs->exists($drupalSettings)) {
-      $oldmask = umask(0);
+      $oldmask = \umask(0);
       $fs->chmod($drupalSettings, 0750);
-      umask($oldmask);
+      \umask($oldmask);
       $io->write('<info>Set sites/default directory to chmod 0750</info>');
     }
     else {
-      $io->warning(sprintf('Missing Drupal settings folder: %s', $drupalSettings));
+      $io->warning(\sprintf('Missing Drupal settings folder: %s', $drupalSettings));
     }
 
     $io->write('<info>Project setup Done! Happy dev!</info>');
